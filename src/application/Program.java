c8d097efa10fbc58b8d4149f591e8746f0f348bb
Bruno Pereira <Brunopereira.19@hotmail.com>
2019-12-29 06:38:16 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,6 @@ package application;
 import java.util.Date;
 import java.util.List;
 
-import javax.swing.JOptionPane;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -16,7 +14,7 @@ public class Program {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== Test 1: Seller findById ===");
+		/*System.out.println("=== Test 1: Seller findById ===");
 		System.out.println(sellerDao.findById(3));
 		//JOptionPane.showMessageDialog(null, sellerDao.findById(3));
 		System.out.println("\n==========================");
@@ -40,10 +38,15 @@ public class Program {
 		System.out.println("\n=== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
-	
-	}	
+		System.out.println("Inserted! New id = " + newSeller.getId());*/
 		
-				
+		System.out.println("\n=== TEST 5: seller update =====");
+		Seller seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		
+		
+	}	
 	}
-
